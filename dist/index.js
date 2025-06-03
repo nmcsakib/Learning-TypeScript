3c27e5948b372b05b5d7c56aa0ad0b3c4d2917e1
nmcsakib <nmcsakib.1@gmail.com>
2025-06-03 16:46:29 +0600

    learned TS dom manupulation

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Hello world");
+const form = document.getElementById('user-form');
+console.log(form);
+// const handleSubmit = (e: { target: HTMLFormElement }) => {
+//     e.target.preventDefault()
+//     const form = new FormData(e.target);
+//     const name = form.get('userName') as FormDataEntryValue
+//     const password = form.get('password') as FormDataEntryValue
+//     console.log(name, password);
+// }
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const name = form.get('userName');
+    const password = form.get('password');
+    console.log(name, password);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world");
+const form = document.getElementById('user-form') as HTMLFormElement;
+console.log(form);
+
+// const handleSubmit = (e: { target: HTMLFormElement }) => {
+//     e.target.preventDefault()
+//     const form = new FormData(e.target);
+//     const name = form.get('userName') as FormDataEntryValue
+//     const password = form.get('password') as FormDataEntryValue
+//     console.log(name, password);
+// }
+
+form.addEventListener('submit',(event: Event) => {
+    event.preventDefault()
+
+    const form = new FormData(event.target as HTMLFormElement);
+    const name = form.get('userName') as FormDataEntryValue
+    const password = form.get('password') as FormDataEntryValue
+    console.log(name, password);
+})
